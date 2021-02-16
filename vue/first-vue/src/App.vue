bb942c06ca84eed9d2e4e43d924c09dd259b994e
Alperen Şahin <alperensahin@Alperens-MacBook-Pro.local>
2021-02-16 14:33:21 +0300

    vue

---
+++ vue/first-vue/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <MyComponent name="alperen"></MyComponent>
+    <MyComponent name="bektas"></MyComponent>
+  </div>
+</template>
+
+<script>
+
+import MyComponent from "@/components/MyComponent";
+
+export default {
+  name: 'App',
+  components: {
+    MyComponent
+  }
+}
+
+</script>
+
+<style>
+body{
+  font-family: Arial;
+}
+</style>
